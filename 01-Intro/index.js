9300e504960dda40abaf740bbb4f7868a7664860
Vaughn <vseekamp@gmail.com>
2023-11-13 11:14:45 -0800

    so rusty

---
+++ 01-Intro/index.js
@@ -0,0 +1,16 @@
+class Hello extends React.Component {
+	render() {
+		return (
+			// Comment the h1 in to see that we can't have multiple top level elements.
+			// <h1>This wont work</h1>
+			// Instead, multiple h1s are inside of a single top level div.
+			<div>
+				<h1>Hello there!</h1>
+				<h1>Hello there!</h1>
+				<h1>Hello there!</h1>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
