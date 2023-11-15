969c4c2247b7da210050e4d6a61d23a711f845be
Vaughn <vseekamp@gmail.com>
2023-11-15 10:30:05 -0800

    fruits-exercise

---
+++ 04-CreateReactApp/fruits-exercise/src/index.js
@@ -0,0 +1,13 @@
+import fruits from "./foods"
+import { choice, remove } from "./helpers"
+
+function pickAndGiveFruit(fruits) {
+    let fruit = choice(fruits);
+    console.log(`I'd like one ${fruit}, please.`);
+    console.log(`Here you go: ${fruit}`);
+    console.log(`Delicious! May I have another?`);
+    let fruitsLeft = remove(fruits, fruit)
+    console.log(`I'm sorry, we're all out. We have ${fruitsLeft.length} other fruits left.`)
+}
+
+pickAndGiveFruit(fruits);
