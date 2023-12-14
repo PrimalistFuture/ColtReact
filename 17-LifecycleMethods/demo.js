074ce96ae40f5ec289cb8e05a7997467730b77e8
Vaughn <vseekamp@gmail.com>
2023-12-14 12:15:10 -0800

    componentDidUpdate

--- 17-LifecycleMethods/demo.js
+++ 17-LifecycleMethods/demo.js
@@ -23,26 +23,77 @@ class Timer extends Component {
 }
 
 // Loading DATA via AJAX
-
+// animated loader
 class ZenQuote extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            quote: ''
+            quote: '',
+            isLoaded: false,
         }
     }
     componentDidMount() {
         // load data
-        // set state with that data
+        // set state with that data, including isLoaded
         axios.get('some_url').then(response => {
-            this.setState({ quote: response.data })
+            this.setState({ quote: response.data, isLoaded: true })
+        })
+    }
+    componentDidUpdate(prevProps, prevState) {
+        // doesn't make a lot of sense for this component...
+    }
+
+    componentWillUnmount() {
+        // clean stuff up
+    }
+    render() {
+        return (
+            <div>
+                {this.state.isLoaded ?
+                    <div>
+                        <h1>Always remember...</h1>
+                        <p>{this.state.quote}</p>
+                    </div>
+                    :
+                    <div className="loader" />
+                }
+            </div>
+        )
+    }
+}
+
+
+// loading data with async functions
+class GitHubUserInfo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            imgUrl: '',
+            name: '',
+            isLoaded: false
+        }
+    }
+    async componentDidMount() {
+        const url = `https://api.github.com/users/${this.props.username}`;
+        let response = await axios.get(url);
+        let data = response.data;
+        this.setState({
+            imgUrl: data.avatar_url,
+            name: data.name,
+            isLoaded: true
         })
     }
     render() {
         return (
             <div>
-                <h1>Always remember...</h1>
-                <p>{this.state.quote}</p>
+                {this.state.isLoaded ?
+                    <div>
+                        <h1>User: {this.state.name}</h1>
+                        <img src={this.state.imgUrl} />
+                    </div>
+                    :
+                    <div className="Loading" />
+                }
             </div>
         )
     }
