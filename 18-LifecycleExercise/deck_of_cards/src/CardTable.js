6b54de374c99c2aa11ae72ab310d2d4e74bf4f54
Vaughn <vseekamp@gmail.com>
2023-12-15 10:57:33 -0800

    deck done

--- 18-LifecycleExercise/deck_of_cards/src/CardTable.js
+++ 18-LifecycleExercise/deck_of_cards/src/CardTable.js
@@ -1,43 +1,61 @@
 import React, { Component } from "react";
-import axios from 'axios'
+import axios from 'axios';
 import Card from './Card';
+import './CardTable.css';
+const API_BASE_URL = `https://deckofcardsapi.com/api/deck`;
+
 class CardTable extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            deckID: '',
-            cardsRemaining: 0,
-            cardImg: '',
+            deck: null,
+            drawn: []
         }
         this.handleClick = this.handleClick.bind(this);
     }
     async componentDidMount() {
-        const url = `https://deckofcardsapi.com/api/deck/new/shuffle/`;
-        let response = await axios.get(url);
-        let deckID = response.data.deck_id;
-        let cardsRemaining = response.data.remaining;
+        let response = await axios.get(`${API_BASE_URL}/new/shuffle/`);
         this.setState({
-            deckID: deckID,
-            cardsRemaining: cardsRemaining
+            deck: response.data
         })
     }
 
     async handleClick(e) {
-        const url = `https://deckofcardsapi.com/api/deck/${this.state.deckID}/draw/`
-        let response = await axios.get(url);
-        let cardImg = response.data.cards[0].image;
-        let cardsRemaining = response.data.remaining;
-        this.setState({
-            cardsRemaining: cardsRemaining,
-            cardImg: cardImg
-        })
+        let deckID = this.state.deck.deck_id;
+        try {
+            let cardUrl = `${API_BASE_URL}/${deckID}/draw/`;
+            let response = await axios.get(cardUrl);
+            if (!response.data.success) {
+                throw new Error(`No cards Remaining`)
+            }
+            let card = response.data.cards[0];
+            this.setState(st => ({
+                drawn: [
+                    ...st.drawn,
+                    {
+                        id: card.code,
+                        image: card.image,
+                        name: `${card.value} of ${card.suit}`
+                    }
+                ]
+            }))
+        } catch (error) {
+            alert(error);
+        }
+
     }
 
     render() {
+        const cards = this.state.drawn.map(c => {
+            return <Card key={c.id} name={c.name} image={c.image} />
+        });
         return (
-            <div>
-                {this.state.cardsRemaining ? <button onClick={this.handleClick}>Draw a card!</button> : ''}
-                <h1><Card image={this.state.cardImg} /></h1>
+            <div className="CardTable">
+                <h1 className="CardTable-title">Card Dealer</h1>
+                <button className='CardTable-btn' onClick={this.handleClick}>Draw a card!</button>
+                <div className="CardTable-cardarea">
+                    {cards}
+                </div>
             </div>
         )
     }
