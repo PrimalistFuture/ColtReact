1858d205b5615afd566298fbd54b879668ccba5e
Vaughn <vseekamp@gmail.com>
2023-12-18 15:04:06 -0800

    didnt do animations

--- 19-DadJokeExercise/dad_joke/src/ColtJokeList.js
+++ 19-DadJokeExercise/dad_joke/src/ColtJokeList.js
@@ -13,8 +13,10 @@ class ColtJokeList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            jokes: JSON.parse(window.localStorage.getItem('jokes') || "[]")
+            jokes: JSON.parse(window.localStorage.getItem('jokes') || "[]"),
+            loading: false,
         };
+        this.seenJokes = new Set(this.state.jokes.map(j => j.text));
         this.handleClick = this.handleClick.bind(this);
     }
 
@@ -25,25 +27,33 @@ class ColtJokeList extends Component {
     }
 
     async getJokes() {
-        let jokes = [];
-        while (jokes.length < this.props.numJokesToGet) {
-            let res = await axios.get(API_URL, {
-                headers: { Accept: "application/json" }
-            });
-            jokes.push(
-                {
-                    id: uuid(),
-                    text: res.data.joke,
-                    votes: 0,
+        try {
+            let jokes = [];
+            while (jokes.length < this.props.numJokesToGet) {
+                let res = await axios.get(API_URL, {
+                    headers: { Accept: "application/json" }
+                });
+                if (!this.seenJokes.has(res.data.joke)) {
+                    jokes.push(
+                        {
+                            id: uuid(),
+                            text: res.data.joke,
+                            votes: 0,
+                        }
+                    );
                 }
-            );
-        }
-        this.setState(st => ({
-            jokes: [...st.jokes, ...jokes]
-        }),
-            () => window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes)));
+            }
+            this.setState(st => ({
+                jokes: [...st.jokes, ...jokes],
+                loading: false
+            }),
+                () => window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes)));
 
-        window.localStorage.setItem("jokes", JSON.stringify(jokes));
+            window.localStorage.setItem("jokes", JSON.stringify(jokes));
+        } catch (e) {
+            alert(e);
+            this.setState({ loading: false })
+        }
     }
 
     handleVote(id, delta) {
@@ -58,10 +68,21 @@ class ColtJokeList extends Component {
     }
 
     handleClick() {
-        this.getJokes();
+        this.setState({
+            loading: true
+        }, this.getJokes);
     }
 
     render() {
+        if (this.state.loading) {
+            return (
+                <div className="ColtJokeList-spinner">
+                    <i className="far fa-8x fa-laugh fa-spin" />
+                    <h1 className="ColtJokeList-title">Loading...</h1>
+                </div>
+            );
+        }
+        let jokes = this.state.jokes.sort((a, b) => b.votes - a.votes);
         return (
             <div className="ColtJokeList">
                 <div className="ColtJokeList-sidebar">
@@ -69,11 +90,11 @@ class ColtJokeList extends Component {
                         <span>Dad</span> Jokes
                     </h1>
                     <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" alt="crying laughing face" />
-                    <button className="ColtJokeList-getmore" onClick={this.handleClick}>Get More Jokes</button>
+                    <button className="ColtJokeList-getmore" onClick={this.handleClick}>Fetch Jokes</button>
                 </div>
 
                 <div className="ColtJokeList-jokes">
-                    {this.state.jokes.map(j => (
+                    {jokes.map(j => (
                         <Joke
                             key={j.id}
                             votes={j.votes}
