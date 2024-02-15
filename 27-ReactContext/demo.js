e30014403d148acf4444f321cd8d71286361f077
Vaughn <vseekamp@gmail.com>
2024-02-15 09:21:47 -0800

    context consumers

--- 27-ReactContext/demo.js
+++ 27-ReactContext/demo.js
@@ -60,10 +60,12 @@ class App extends Component {
 
 // usually contexts go in their own folder
 
-import React, { createContext } from "react";
+import React, { Component, createContext } from "react";
 
+//export this
 const ThemeContext = createContext();
 
+// export this
 class ThemeProvider extends Component {
     constructor(props) {
         super(props);
@@ -77,3 +79,80 @@ class ThemeProvider extends Component {
         )
     }
 }
+
+// class-based Context Consumer 
+// import {ThemeContext}
+
+class NavbarWithContext extends Component {
+    //this tells this class to look up and see if it is wrapped in a Context Provider. What it finds will be called 'context'. 
+    static contextType = ThemeContext;
+    render() {
+        console.log(this.context);
+        const { isDarkMode } = this.context;
+        return (
+            <Appbar position='static' color={isDarkMode ? "black" : "white"}>
+                {/* Whatever */}
+            </Appbar>
+        )
+    }
+}
+
+// Language Context
+
+import React, { Component, createContext } from "react";
+
+// export this
+const LanguageContext = createContext();
+// export this
+class LanguageProvider extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { language: "french" }
+        this.changeLanguage = this.changeLanguage.bind(this);
+    }
+    //this would go in some input menu
+    changeLanguage(e) {
+        this.setState({ language: e.target.value })
+    }
+    render() {
+        return (
+            <LanguageContext.Provider value={{ ...this.state, changeLanguage: this.changeLanguage }}>
+                {this.props.children}
+            </LanguageContext.Provider>
+        )
+    }
+}
+
+// Language Context Consumer
+// this variable would have a bunch of the different words in their respective languages.
+const words = {
+    english: {
+        email: "Email"
+    },
+    french: {
+        email: "Adresse Electronique"
+    },
+    spanish: {
+        email: "Correo Electronico"
+    }
+};
+
+class Form extends Component {
+    static contextType = LanguageContext;
+    render() {
+        const { language } = this.context;
+        const { email } = words[language];
+        return (
+            <Input>{email}</Input>
+        )
+    }
+}
+
+// Another way to provide context is by making a higher-order component and passing another component into that, which will now have access to that context
+const withLanguageContext = Component => props => {
+    <LanguageContext.Consumer>
+        {value => <Component LanguageContext={value} {...props} />}
+    </LanguageContext.Consumer>
+}
+
+// export default withLanguageContext(Navbar);
